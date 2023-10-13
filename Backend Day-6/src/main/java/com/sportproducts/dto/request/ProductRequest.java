7b2372fc9aaeb147dfb98734702040b6fc1259b2
Harsita B <116861905+harsibaski@users.noreply.github.com>
2023-10-13 14:28:32 +0530

    Add files via upload

---
+++ Backend Day-6/src/main/java/com/sportproducts/dto/request/ProductRequest.java
@@ -0,0 +1,18 @@
+package com.sportproducts.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+    private String productName;
+    private float productPrice;
+    private String productImage;
+    private String description;
+    private long productQuantity;
+}
