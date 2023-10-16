6a0148acf19457371d916396c3b1d11a8704058d
Harsita B <116861905+harsibaski@users.noreply.github.com>
2023-10-16 12:19:01 +0530

    Add files via upload

---
+++ Backend-Day-13/prosportemporium2/src/main/java/com/sportproducts/ProsportemporiumApplication.java
@@ -0,0 +1,30 @@
+package com.sportproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+		info =@Info(
+				title = "Pro Sport Emporium",
+				version = "1.1.2",
+				description = "Sports E-commerce Website Backend",
+				contact = @Contact(
+						name = "Guhan",
+						email = "jbguhan@gmail.com"
+						)
+				)
+		)
+@EnableFeignClients
+@SpringBootApplication
+public class ProsportemporiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProsportemporiumApplication.class, args);
+	}
+
+}
---
+++ Backend-Day-13/prosportemporium2/src/main/java/com/sportproducts/repository/OrderMappingRepository.java
@@ -0,0 +1,17 @@
+package com.sportproducts.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sportproducts.model.Order;
+import com.sportproducts.model.OrderMapping;
+
+
+
+public interface OrderMappingRepository extends JpaRepository<OrderMapping, Long> {
+	 void deleteByProductPid(Long productPid);
+
+	void deleteByOrderOid(Long orderId);
+
+}
